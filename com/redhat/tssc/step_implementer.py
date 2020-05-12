633758f6bd2c0aa104705f59c3f3a9e738830812
Ian Tewksbury <itewk@redhat.com>
2020-05-12 19:52:34 -0400

    step_implementer.py - add constants for the default TSSC workflow steps

--- com/redhat/tssc/step_implementer.py
+++ com/redhat/tssc/step_implementer.py
@@ -11,6 +11,22 @@ class DefaultSteps: # pylint: disable=too-few-public-methods
     GENERATE_METADATA = 'generate-metadata'
     TAG_SOURCE = 'tag-source'
     SECURITY_STATIC_CODE_ANALYSIS = 'security-static-code-analysis'
+    LINTING_STATIC_CODE_ANALYSIS = 'security-static-code-analysis'
+    PACKAGE = 'package'
+    UNIT_TEST = 'unit-test'
+    PUSH_ARTIFACTS = 'push-artifacts'
+    CREATE_CONTAINER_IMAGE = 'create-container'
+    PUSH_CONTAINER_IMAGE = 'push-container-image'
+    CONTAINER_IMAGE_UNIT_TEST = 'container-image-unit-test'
+    CONTAINER_IMAGE_STATIC_COMPLIANCE_SCAN = 'container-image-static-compliance-scan'
+    CONTAINER_IMAGE_STATIC_ULNERABILITY_SCAN = 'container-image-static-vulnerability-scan'
+    PUSH_TRUSTED_CONTAINER_IMAGE = 'push-trusted-container-image'
+    CREATE_KUBE_PROJECT = 'create-kube-project'
+    DEPLOY_TO_KUBE_PROJECT = 'deploy-to-kube-project'
+    UAT = 'uat'
+    RUNTIME_VULNERABILITY_SCAN = 'runtime-vulnerability-scan'
+    CANARY_TEST = 'canary-test'
+    PUBLISH_WROKFLOW_RESULTS = 'publish-workflow-results'
 
 class StepImplementer(ABC): # pylint: disable=too-few-public-methods
     """
