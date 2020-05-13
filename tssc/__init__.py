2eb19cf5169283ada169d9ec34ae375f9c2dd70e
Ian Tewksbury <itewk@redhat.com>
2020-05-13 16:23:15 -0400

    tssc/__init__.py - docs - add examples

--- tssc/__init__.py
+++ tssc/__init__.py
@@ -3,7 +3,18 @@ Trusted Software Supply Chain Library (tssc-lib) main entry point.
 
 Examples
 --------
-Bla
+
+Getting Help
+
+>>> python -m tssc --help
+
+
+Example Running the 'generate-metadata' step
+
+>>> python -m tssc
+...     --config-file=my-app-tssc-config.yml
+...     --results-file=my-app-tssc-results.yml
+...     --step=generate-metadata
 
 """
 
