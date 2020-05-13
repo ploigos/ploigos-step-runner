3718feee9c137767408f571f19e3f48a27d4e87e
Ian Tewksbury <itewk@redhat.com>
2020-05-13 13:54:05 -0400

    tests/com/redhat/tssc/test_factory.py - add some basic tests for running steps

--- com/redhat/tssc/factory.py
+++ com/redhat/tssc/factory.py
@@ -7,7 +7,7 @@ _TSSC_CONFIG_KEY = 'tssc-config'
 _IS_DEFAULT_KEY = 'is_default'
 _CLAZZ_KEY = 'clazz'
 _IMPLEMENTER_KEY = 'implementer'
-_STEP_CONFIG_KEY = 'config'
+_SUB_STEP_CONFIG_KEY = 'config'
 
 class TSSCFactory:
     """
@@ -18,6 +18,10 @@ class TSSCFactory:
     config : dict
         TSSC configuration represented as a dictionary with a key of 'tssc-config'
 
+    Raises
+    ------
+    ValueError
+        If given config does not contain 'tssc-config' key
     """
     _step_implementers = dict()
 
@@ -50,7 +54,8 @@ class TSSCFactory:
         # if this is the default, unset any other implimenters of this step as default
         # NOTE: last one to register as default wins, deal with it
         if is_default:
-            for step_implementer in TSSCFactory._step_implementers[step_name]:
+            for step_implementer in \
+                    TSSCFactory._step_implementers[step_name].values():
                 step_implementer[_IS_DEFAULT_KEY] = False
 
         TSSCFactory._step_implementers[step_name][implementer_class.__name__] = {
@@ -58,7 +63,7 @@ class TSSCFactory:
             _IS_DEFAULT_KEY: is_default
         }
 
-    def run_step(self, step_name):
+    def run_step(self, step_name): # pylint: disable=too-many-branches
         """
         Call the given step.
 
@@ -66,6 +71,14 @@ class TSSCFactory:
         ----------
         step_name : str
             TSSC step to run.
+
+        Raises
+        ------
+        TSSCException
+            If no StepImplementers have been registered for the given step_name
+            If no specfiic StepImplementer name specified in sub step config
+                and no default StepImplementer registered for given step_name.
+            If no StepImplementer regsitered for given step with given implemeneter name.
         """
 
         # verify that there is registered implementers for the given step
@@ -83,15 +96,30 @@ class TSSCFactory:
         if step_name in self.config:
             step_config = self.config[step_name]
 
-            for sub_step_config in step_config:
-                step_implementer_name = sub_step_config[_IMPLEMENTER_KEY]
-                sub_step = step_implementers[step_implementer_name][_CLAZZ_KEY](
-                    sub_step_config[_STEP_CONFIG_KEY]
-                )
-                sub_step.run_step()
+            for sub_step in step_config:
+                sub_step_implementer_name = sub_step[_IMPLEMENTER_KEY]
+
+                if sub_step_implementer_name in step_implementers:
+                    if _SUB_STEP_CONFIG_KEY in sub_step:
+                        sub_step_config = sub_step[_SUB_STEP_CONFIG_KEY]
+                    else:
+                        sub_step_config = dict()
+
+                    sub_step = step_implementers[sub_step_implementer_name][_CLAZZ_KEY](
+                        sub_step_config
+                    )
+                    sub_step.run_step()
+                else:
+                    raise TSSCException(
+                        'No StepImplementer for step'
+                        + ' (' + step_name + ')'
+                        + ' with TSSC config specified implementer name'
+                        + ' (' + sub_step_implementer_name + ')'
+                    )
+
         else:
             default_step_implementer = None
-            for step_implementer_name, step_implementer_config in step_implementers.items():
+            for sub_step_implementer_name, step_implementer_config in step_implementers.items():
                 if step_implementer_config[_IS_DEFAULT_KEY]:
                     default_step_implementer = step_implementer_config
 
