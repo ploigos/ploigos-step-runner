4ac113c8e501f039279a11197e001b1d95f84953
Ian Tewksbury <itewk@redhat.com>
2020-05-13 17:03:10 -0400

    setup.py - init add

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tssc",
+    version="0.1",
+    author="Red Hat Services",
+    description="Trusted Software Supply Chain (TSSC) python library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rhtconsulting/tssc-python-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
