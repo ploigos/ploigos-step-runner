a629dabe0e6034e0834a910a948ac194790fdafe
Ian Tewksbury <itewk@redhat.com>
2020-05-13 15:03:47 -0400

    move tssc.py to com/redhat/tssc/__main__.py

--- com/redhat/tssc/__init__.py
+++ com/redhat/tssc/__init__.py
@@ -2,11 +2,13 @@
 com.redhat.tssc
 """
 
+import __main__
 from .factory import TSSCFactory
 from .exceptions import TSSCException
 from .step_implementer import DefaultSteps, StepImplementer
 
 __all__ = [
+    '__main__',
     'factory',
     'exceptions',
     'step_implementer'
