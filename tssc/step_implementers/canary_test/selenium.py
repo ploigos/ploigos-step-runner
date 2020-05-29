87adb8aeae995f68a144744abcd5512ebe8853bb
Ian Tewksbury <itewk@redhat.com>
2020-05-29 13:31:00 -0400

    tssc/step_implementers/canary_test/selenium.py - initial stub add

---
+++ tssc/step_implementers/canary_test/selenium.py
@@ -0,0 +1,40 @@
+"""
+Step Implementer for the canary-test step for Selenium.
+"""
+
+from tssc import TSSCFactory
+from tssc import StepImplementer
+from tssc import DefaultSteps
+
+DEFAULT_ARGS = {}
+
+class Selenium(StepImplementer):
+    """
+    StepImplementer for the canary-test step for Selenium.
+    """
+
+    def __init__(self, config, results_file):
+        super().__init__(config, results_file, DEFAULT_ARGS)
+
+    @classmethod
+    def step_name(cls):
+        return DefaultSteps.CANARY_TEST
+
+    def _validate_step_config(self, step_config):
+        """
+        Function for implementers to override to do custom step config validation.
+
+        Parameters
+        ----------
+        step_config : dict
+            Step configuration to validate.
+        """
+
+    def _run_step(self, runtime_step_config):
+        results = {
+        }
+
+        return results
+
+# register step implementer
+TSSCFactory.register_step_implementer(Selenium)
