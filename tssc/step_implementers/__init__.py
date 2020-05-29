ddfc9473864390a8a148b38013a529112e9e1256
Ian Tewksbury <itewk@redhat.com>
2020-05-29 10:36:08 -0400

    tssc/step_implementers/package/maven.py - init add

--- tssc/step_implementers/__init__.py
+++ tssc/step_implementers/__init__.py
@@ -5,9 +5,13 @@ com.redhat.tssc.step_implementers
 from .generate_metadata import *
 from .tag_source import *
 from .security_static_code_analysis import *
+from .linting_static_code_analysis import *
+from .package import *
 
 __all__ = [
     'generate_metadata',
     'tag_source',
-    'security_static_code_analysis'
+    'security_static_code_analysis',
+    'linting_static_code_analysis',
+    'package'
 ]
