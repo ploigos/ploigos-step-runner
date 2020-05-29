741e3305739c8de35ca19581124e1d3879407dd2
Ian Tewksbury <itewk@redhat.com>
2020-05-29 13:07:46 -0400

    tssc/step_implementers/container_image_static_compliance_scan/openscap.py and tssc/step_implementers/container_image_static_vulnerability_scan/openscap.py - init stub add

--- tssc/step_implementers/__init__.py
+++ tssc/step_implementers/__init__.py
@@ -8,6 +8,8 @@ from .security_static_code_analysis import *
 from .linting_static_code_analysis import *
 from .package import *
 from .unit_test import *
+from .container_image_static_compliance_scan import *
+from .container_image_static_vulnerability_scan import *
 
 __all__ = [
     'generate_metadata',
@@ -15,5 +17,7 @@ __all__ = [
     'security_static_code_analysis',
     'linting_static_code_analysis',
     'package',
-    'unit_test'
+    'unit_test',
+    'container_image_static_compliance_scan',
+    'container_image_static_vulnerability_scan'
 ]
