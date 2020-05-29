a79744e8aa2e2d457cc98e7307db88ad07792db7
Ian Tewksbury <itewk@redhat.com>
2020-05-29 10:13:53 -0400

    tssc/step_implementers/security_static_code_analysis/sonarqube.py - init add with tests

--- tssc/step_implementers/__init__.py
+++ tssc/step_implementers/__init__.py
@@ -4,8 +4,10 @@ com.redhat.tssc.step_implementers
 
 from .generate_metadata import *
 from .tag_source import *
+from .security_static_code_analysis import *
 
 __all__ = [
     'generate_metadata',
-    'tag_source'
+    'tag_source',
+    'security_static_code_analysis'
 ]
---
+++ tssc/step_implementers/security_static_code_analysis/sonarqube.py
@@ -0,0 +1,40 @@
+"""
+Step Implementer for the tag-source step for SonarQube.
+"""
+
+from tssc import TSSCFactory
+from tssc import StepImplementer
+from tssc import DefaultSteps
+
+DEFAULT_ARGS = {}
+
+class SonarQube(StepImplementer):
+    """
+    StepImplementer for the tag-source step for SonarQube.
+    """
+
+    def __init__(self, config, results_file):
+        super().__init__(config, results_file, DEFAULT_ARGS)
+
+    @classmethod
+    def step_name(cls):
+        return DefaultSteps.SECURITY_STATIC_CODE_ANALYSIS
+
+    def _validate_step_config(self, step_config):
+        """
+        Function for implementers to override to do custom step config validation.
+
+        Parameters
+        ----------
+        step_config : dict
+            Step configuration to validate.
+        """
+
+    def _run_step(self, runtime_step_config):
+        results = {
+        }
+
+        return results
+
+# register step implementer
+TSSCFactory.register_step_implementer(SonarQube)
