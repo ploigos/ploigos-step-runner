3791af4c63fec452a7f08fc7fb7e7b2352b93c25
Kevin J. Franklin <kfrankli@redhat.com>
2020-06-02 13:56:37 -0400

    NAPSSPO-330 Basic subprocess call to mvn clean install working for package

--- tssc/step_implementers/package/maven.py
+++ tssc/step_implementers/package/maven.py
@@ -1,16 +1,45 @@
 """
 Step Implementer for the package step for Maven.
 """
+import os
+import subprocess
 
 from tssc import TSSCFactory
 from tssc import StepImplementer
 from tssc import DefaultSteps
 
-DEFAULT_ARGS = {}
+DEFAULT_ARGS = {
+    'pom-file': 'pom.xml'
+}
+
+# https://stackoverflow.com/questions/21377520/do-a-maven-build-in-a-python-script
+class ChangeDir:
+    """
+    Internal helper class to manage changing directories, and returning to the
+    starting working directory.
+    """
+    def __init__(self, new_path):
+        self.saved_path = None
+        self.new_path = os.path.expanduser(new_path)
+
+    # Change directory with the new path
+    def __enter__(self):
+        self.saved_path = os.getcwd()
+        os.chdir(self.new_path)
+
+    # Return back to previous directory
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.saved_path)
 
 class Maven(StepImplementer):
     """
     StepImplementer for the package step for Maven.
+
+    Raises
+    ------
+    ValueError
+        If given pom file does not exist
+        If given pom file does not contain required elements
     """
 
     def __init__(self, config, results_file):
@@ -29,11 +58,23 @@ class Maven(StepImplementer):
         step_config : dict
             Step configuration to validate.
         """
+        print(step_config)
+
+        if 'pom-file' not in step_config or not step_config['pom-file']:
+            raise ValueError('Key (pom-file) must have none empty value in the step configuration')
 
     def _run_step(self, runtime_step_config):
+        pom_file = runtime_step_config['pom-file']
+
+        process_args = ["mvn", "clean", "install"]
+
+        with ChangeDir(os.path.dirname(pom_file)):
+            return_code = subprocess.call(process_args)
+        if return_code:
+            raise ValueError('Issue invoking ' + str(process_args) + \
+              ' with given pom file (' + pom_file + ')')
         results = {
         }
-
         return results
 
 # register step implementer
