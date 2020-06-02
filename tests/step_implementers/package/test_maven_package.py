f5e257a8c12a4141f3f4ac843a079c70b33c82cc
Kevin J. Franklin <kfrankli@redhat.com>
2020-06-02 13:56:37 -0400

    NAPSSPO-330 Basic subprocess call to mvn clean install working for package

--- tests/step_implementers/package/test_maven_package.py
+++ tests/step_implementers/package/test_maven_package.py
@@ -11,11 +11,20 @@ from test_utils import *
 
 def test_tag_source_specify_git_implementer():
     with TempDirectory() as temp_dir:
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>com.mycompany.app</groupId>
+    <artifactId>my-app</artifactId>
+    <version>42.1</version>
+</project>''')
+        pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
         config = {
-            'tssc-config': {    
+            'tssc-config': {
                 'package': {
                     'implementer': 'Maven',
-                    'config': {}
+                    'config': {
+                        'pom-file': str(pom_file_path)
+                    }
                 }
             }
         }
