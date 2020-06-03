aa5a5aaa581abf97848345fa029fb9c3d841ec06
Kevin J. Franklin <kfrankli@redhat.com>
2020-06-02 22:30:19 -0400

    NAPSSPO-339 Got basic test case of single jar mvn build working successfully

--- tests/step_implementers/package/test_maven_package.py
+++ tests/step_implementers/package/test_maven_package.py
@@ -9,13 +9,22 @@ from tssc.step_implementers.package import Maven
 
 from test_utils import *
 
-def test_tag_source_specify_git_implementer():
+
+def test_package_java_mvn_quickstart_single_jar():
     with TempDirectory() as temp_dir:
+        #java_app_class_file = os.path.join(temp_dir.path,"src/main/java/com/mycompany/app/app.java")
+        #os.makedirs(java_app_class_file)
+        temp_dir.write('src/main/java/com/mycompany/app/App.java',b'''package com.mycompany.app;
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}''')
         temp_dir.write('pom.xml',b'''<project>
     <modelVersion>4.0.0</modelVersion>
     <groupId>com.mycompany.app</groupId>
     <artifactId>my-app</artifactId>
-    <version>42.1</version>
+    <version>1.0-SNAPSHOT</version>
 </project>''')
         pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
         config = {
@@ -28,6 +37,14 @@ def test_tag_source_specify_git_implementer():
                 }
             }
         }
-        expected_step_results = {'tssc-results': {'package': {}}}
+        expected_step_results = {
+            'tssc-results': {
+                'package': {
+                    'artifacts': {
+                        'my-app-1.0-SNAPSHOT.jar': os.path.join(temp_dir.path, 'my-app-1.0-SNAPSHOT.jar')
+                    }
+                }
+            }
+        }
 
         run_step_test_with_result_validation(temp_dir, 'package', config, expected_step_results)
