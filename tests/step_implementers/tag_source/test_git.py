8130f27357ca30a3cc02b20f10d4051094cb66c5
Kevin J. Franklin <kfrankli@redhat.com>
2020-07-21 12:31:22 -0400

    NAPSSPO-487 wip of tag source

--- tests/step_implementers/tag_source/test_git.py
+++ tests/step_implementers/tag_source/test_git.py
@@ -3,17 +3,23 @@ import os
 import pytest
 from testfixtures import TempDirectory
 import yaml
+import mock
 
 from tssc import TSSCFactory
 from tssc.step_implementers.tag_source import Git
 
 from test_utils import *
 
+def mock_git_tag(tag):
+    return True
+
 def test_tag_source_default():
     with TempDirectory() as temp_dir:
         config = {
             'tssc-config': {}
         }
+        Git._git_tag = mock_git_tag
+
         expected_step_results = {'tssc-results': {'tag-source': {}}}
 
         run_step_test_with_result_validation(temp_dir, 'tag-source', config, expected_step_results)
@@ -21,7 +27,7 @@ def test_tag_source_default():
 def test_tag_source_specify_git_implementer():
     with TempDirectory() as temp_dir:
         config = {
-            'tssc-config': {    
+            'tssc-config': {
                 'tag-source': {
                     'implementer': 'Git',
                     'config': {}
--- tssc/step_implementers/tag_source/git.py
+++ tssc/step_implementers/tag_source/git.py
@@ -1,7 +1,9 @@
 """
 Step Implementer for the tag-source step for Git.
 """
-
+import sys
+import sh
+import os
 from tssc import TSSCFactory
 from tssc import StepImplementer
 from tssc import DefaultSteps
@@ -31,10 +33,61 @@ class Git(StepImplementer):
         """
 
     def _run_step(self, runtime_step_config):
+        username = None
+        password = None
+        tag = 'latest'
+        if(runtime_step_config.get('username_env_var') \
+          and runtime_step_config.get('password_env_var')):
+            username = os.getenv(runtime_step_config.get('username_env_var'))
+            password = os.getenv(runtime_step_config.get('password_env_var'))
+        else:
+            print('No username/password found, assuming ssh')
+        if(self.get_step_results('generate-metadata') \
+          and self.get_step_results('generate-metadata').get('image-tag')):
+            tag = self.get_step_results('generate-metadata').get('image-tag')
+        else:
+            print('No version found in metadata. Using latest')
+        self._git_tag(tag)
+        git_url = self._git_url(runtime_step_config)
+        self._git_push(git_url, username, password)
         results = {
+            'git_tag' : tag
         }
-
         return results
+
+    def _git_url(self, runtime_step_config):
+        returnVal = None
+        if(runtime_step_config.get('git_url')):
+            returnVal = runtime_step_config.get('git_url')
+        else:
+            git_config = sh.git.bake("config")
+            try:
+                returnVal = git_config(
+                    '--get',
+                    'remote.origin.url',
+                    _out=sys.stdout)
+            except sh.ErrorReturnCode:  # pylint: disable=undefined-variable
+                raise RuntimeError('Error invoking git config --get remote.origin.url')
+        return returnVal
+
+    def _git_tag(self, git_tag_value):
+        git_tag = sh.git.bake("tag")
+        try:
+            git_tag(git_tag_value)
+        except:
+            raise RuntimeError('Error invoking git tag ' + git_tag_value)
+
+    def _git_push(self, url, username=None, password=None):
+        git_push = sh.git.bake("push")
+        try:
+            if(username and password):
+                git_push(
+                    'http://' + username + ':' + password + git_url,
+                    '--tag')
+            else:
+                git_push('--tag')
+        except sh.ErrorReturnCode:  # pylint: disable=undefined-variable
+            raise RuntimeError('Error invoking git push')
 
 # register step implementer
 TSSCFactory.register_step_implementer(Git, True)
