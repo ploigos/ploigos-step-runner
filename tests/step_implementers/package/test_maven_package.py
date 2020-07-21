e28e6e750643d8de03d9e3e422f2dc57f13d52d1
Kevin J. Franklin <kfrankli@redhat.com>
2020-07-21 17:11:01 -0400

    NAPSSPO-538 created a terrible test for having multiple artifacts

--- tests/step_implementers/package/test_maven_package.py
+++ tests/step_implementers/package/test_maven_package.py
@@ -74,6 +74,90 @@ public class App {
         }
         run_step_test_with_result_validation(temp_dir, 'package', config, expected_step_results)
 
+def test_mvn_multiple_jars():
+    with TempDirectory() as temp_dir:
+        temp_dir.write('src/main/java/com/mycompanya/app/App.java',b'''package com.mycompanya.app;
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}''')
+        temp_dir.write('src/main/java/com/mycompanyb/app/App.java',b'''package com.mycompanyb.app;
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}''')
+        temp_dir.write('pom.xml',b'''<project>
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>com.mycompany.app</groupId>
+    <artifactId>my-app</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <properties>
+        <maven.compiler.source>1.8</maven.compiler.source>
+        <maven.compiler.target>1.8</maven.compiler.target>
+    </properties>
+    <build>
+        <plugins>
+            <plugin>
+                <artifactId>maven-assembly-plugin</artifactId>
+                <executions>
+                    <execution>
+                        <id>jar1</id>
+                        <phase>package</phase>
+                        <goals>
+                            <goal>single</goal>
+                        </goals>
+                        <configuration>
+                            <archive>
+                                <manifest>
+                                    <mainClass>com.mycompanya.app.App</mainClass>
+                                </manifest>
+                            </archive>
+                            <descriptorRefs>
+                                <descriptorRef>jar-with-dependencies</descriptorRef>
+                            </descriptorRefs>
+                            <finalName>companya</finalName>
+                        </configuration>
+                    </execution>
+                    <execution>
+                        <id>jar2</id>
+                        <phase>package</phase>
+                        <goals>
+                            <goal>single</goal>
+                        </goals>
+                        <configuration>
+                            <archive>
+                                <manifest>
+                                    <mainClass>com.mycompanyb.app.App</mainClass>
+                                </manifest>
+                            </archive>
+                            <descriptorRefs>
+                                <descriptorRef>jar-with-dependencies</descriptorRef>
+                            </descriptorRefs>
+                            <finalName>companyb</finalName>
+                        </configuration>
+                    </execution>
+                </executions>
+            </plugin>
+        </plugins>
+    </build>
+</project>''')
+        pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
+        config = {
+            'tssc-config': {
+                'package': {
+                    'implementer': 'Maven',
+                    'config': {
+                        'pom-file': str(pom_file_path)
+                    }
+                }
+            }
+        }
+        factory = TSSCFactory(config)
+        with pytest.raises(ValueError):
+            factory.run_step('package')
+
 def test_pom_file_valid_old_empty_jar ():
     with TempDirectory() as temp_dir:
         temp_dir.write('pom.xml',b'''<project>
