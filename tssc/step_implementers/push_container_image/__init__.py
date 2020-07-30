3db1e3fe916be61b327fe2f8ba8b43bdf3c9985c
Ian Tewksbury <itewk@redhat.com>
2020-07-30 11:38:01 -0400

    NAPSSPO-561 - all steps should have documenation on their expected configuration and results * udpate push container image step implementers doc

--- tssc/step_implementers/push_container_image/__init__.py
+++ tssc/step_implementers/push_container_image/__init__.py
@@ -5,18 +5,20 @@ Step Configuration
 All tssc.StepImplementers for this step should
 accept minimally the following configuration options.
 
-| Parameter       | Description
-|-----------------|------------
-| `TODO`          | TODO
+| Configuration Key | Description
+|-------------------|------------
+| `destination`     | Destination to push image to
+| `src-tls-verify`  | Whether to very TLS for source of image
+| `dest-tls-verify` | Whether to verify TLS for destination of image
 
 Results
 -------
 All tssc.StepImplementers for this step should
 minimally produce the following step results.
 
-| Result Key       | Description
-|------------------|------------
-| `TODO`           | TODO
+| Result Key  | Description
+|-------------|------------
+| `image-tag` | Pushed destination image tag
 """
 
 from .skopeo import Skopeo
