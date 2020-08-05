704b0b00a57071b2f172ad883c35c8467a515f2f
root <kfrankli@redhat.com>
2020-08-05 07:31:00 -0400

    NAPSSPO-558 updating based on input from team

--- tests/step_implementers/push_container_image/test_skopeo.py
+++ tests/step_implementers/push_container_image/test_skopeo.py
@@ -15,29 +15,33 @@ class TestStepImplementerPushContainerImageSkopeo(unittest.TestCase):
         with TempDirectory() as temp_dir:
             application_name = 'foo'
             service_name = 'bar'
+            organization = 'xyzzy'
             config = {
                 'tssc-config': {
                     'global-defaults': {
                         'application-name': application_name,
-                        'service-name': service_name
+                        'service-name': service_name,
+                        'organization': organization
                     }
                 }
             }
             expected_step_results = {'tssc-results': {'push-container-image': {'image-tag': ''}}}
             with self.assertRaisesRegex(
                     AssertionError,
-                    r'The runtime step configuration \(\{\'src-tls-verify\': \'true\', \'dest-tls-verify\': \'true\', \'application-name\': \'foo\', \'service-name\': \'bar\'\}\) is missing the required configuration keys \(\[\'destination\'\]\)'):
+                    r'The runtime step configuration \(\{\'src-tls-verify\': \'true\', \'dest-tls-verify\': \'true\', \'application-name\': \'foo\', \'service-name\': \'bar\', \'organization\': \'xyzzy\'\}\) is missing the required configuration keys \(\[\'destination-url\'\]\)'):
                 run_step_test_with_result_validation(temp_dir, 'push-container-image', config, expected_step_results)
 
     def test_push_container_image_specify_skopeo_implementer_missing_args(self):
         with TempDirectory() as temp_dir:
             application_name = 'foo'
             service_name = 'bar'
+            organization = 'xyzzy'
             config = {
                 'tssc-config': {
                     'global-defaults': {
                         'application-name': application_name,
-                        'service-name': service_name
+                        'service-name': service_name,
+                        'organization': organization
                     },
                     'push-container-image': {
                         'implementer': 'Skopeo',
@@ -49,7 +53,7 @@ class TestStepImplementerPushContainerImageSkopeo(unittest.TestCase):
 
             with self.assertRaisesRegex(
                     AssertionError,
-                    r'The runtime step configuration \(\{\'src-tls-verify\': \'true\', \'dest-tls-verify\': \'true\', \'application-name\': \'foo\', \'service-name\': \'bar\'\}\) is missing the required configuration keys \(\[\'destination\'\]\)'):
+                    r'The runtime step configuration \(\{\'src-tls-verify\': \'true\', \'dest-tls-verify\': \'true\', \'application-name\': \'foo\', \'service-name\': \'bar\', \'organization\': \'xyzzy\'\}\) is missing the required configuration keys \(\[\'destination-url\'\]\)'):
                 run_step_test_with_result_validation(temp_dir, 'push-container-image', config, expected_step_results)
 
     @patch('sh.skopeo', create=True)
@@ -57,16 +61,18 @@ class TestStepImplementerPushContainerImageSkopeo(unittest.TestCase):
         with TempDirectory() as temp_dir:
             application_name = 'foo'
             service_name = 'bar'
+            organization = 'xyzzy'
             config = {
                 'tssc-config': {
                     'global-defaults': {
                         'application-name': application_name,
-                        'service-name': service_name
+                        'service-name': service_name,
+                        'organization': organization
                     },
                     'push-container-image': {
                         'implementer': 'Skopeo',
                         'config': {
-                            'destination' : 'docker-archive:' + temp_dir.path + '/image.tar'
+                            'destination-url' : 'docker-archive:' + temp_dir.path + '/image.tar'
                         }
                     }
                 }
@@ -97,28 +103,30 @@ class TestStepImplementerPushContainerImageSkopeo(unittest.TestCase):
                 )
             application_name = 'foo'
             service_name = 'bar'
+            organization = 'xyzzy'
             config = {
                 'tssc-config': {
                     'global-defaults': {
                         'application-name': application_name,
-                        'service-name': service_name
+                        'service-name': service_name,
+                        'organization': organization
                     },
                     'push-container-image': {
                         'implementer': 'Skopeo',
                         'config': {
                             'source' : source,
-                            'destination' : destination
+                            'destination-url' : destination
                         }
                     }
                 }
             }
-            expected_step_results = {'tssc-results': { 'create-container-image': {'image-tar-file': destination}, 'generate-metadata': {'image-tag': version }, 'push-container-image': {'image-tag': "{destination}/{application_name}/{service_name}:{version}".format(destination=destination, application_name=application_name, service_name=service_name, version=version)}}}
+            expected_step_results = {'tssc-results': { 'create-container-image': {'image-tar-file': destination}, 'generate-metadata': {'image-tag': version }, 'push-container-image': {'image-tag': "{destination}/{organization}/{application_name}-{service_name}:{version}".format(destination=destination, organization=organization, application_name=application_name, service_name=service_name, version=version)}}}
             run_step_test_with_result_validation(temp_dir, 'push-container-image', config, expected_step_results)
             skopeo_mock.copy.assert_called_once_with(
                 '--src-tls-verify=true',
                 '--dest-tls-verify=true',
                 "docker-archive:{destination}".format(destination=destination),
-                "{destination}/{application_name}/{service_name}:{version}".format(destination=destination, application_name=application_name, service_name=service_name, version=version),
+                "{destination}/{organization}/{application_name}-{service_name}:{version}".format(destination=destination, organization=organization, application_name=application_name, service_name=service_name, version=version),
                 _out=sys.stdout
             )
 
@@ -143,17 +151,19 @@ class TestStepImplementerPushContainerImageSkopeo(unittest.TestCase):
 
             application_name = 'foo'
             service_name = 'bar'
+            organization = 'xyzzy'
             config = {
                 'tssc-config': {
                     'global-defaults': {
                         'application-name': application_name,
-                        'service-name': service_name
+                        'service-name': service_name,
+                        'organization': organization
                     },
                     'push-container-image': {
                         'implementer': 'Skopeo',
                         'config': {
                             'source' : source,
-                            'destination' : destination
+                            'destination-url' : destination
                         }
                     }
                 },
