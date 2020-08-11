444ce5232f8c9d8c503b13f31cad0e284e8fc208
Hunter Gerlach <hgerlach@redhat.com>
2020-08-11 19:11:06 -0400

    Add JUnit for unit-test step implementer

--- tests/step_implementers/unit_test/test_junit.py
+++ tests/step_implementers/unit_test/test_junit.py
@@ -22,3 +22,20 @@ def test_tag_source_specify_junit_implementer():
         expected_step_results = {'tssc-results': {'unit-test': {}}}
 
         run_step_test_with_result_validation(temp_dir, 'unit-test', config, expected_step_results)
+
+# Tests:
+## Unit Tests Exist
+## Resulting output with no tests
+## Resulting output with tests
+## Resulting output with errors
+
+## Check for surefire-plugin
+## Check pom for reportsDirectory
+## 
+
+# Not necessary until reading results from mvn cmd
+## Resulting output defining number of tests run
+## Resulting output defining number of test failures
+## Resulting output defining number of test errors
+## Resulting output defining number of tests skipped
+
--- tssc/step_implementers/unit_test/junit.py
+++ tssc/step_implementers/unit_test/junit.py
@@ -7,41 +7,52 @@ Step configuration expected as input to this step.
 Could come from either configuration file or
 from runtime configuration.
 
-| Configuration Key | Required? | Default | Description
-|-------------------|-----------|---------|-----------
-| `TODO`            | True      |         |
+| Configuration Key | Required? | Default     | Description
+|-------------------|-----------|-------------|-----------
+| `pom-file`        | True      | `'pom.xml'` | pom used to check for reportsDirectory definition
 
 Expected Previous Step Results
 ------------------------------
 
 Results expected from previous steps that this step requires.
 
-| Step Name | Result Key | Description
-|-----------|------------|------------
-| `TODO`    | `TODO`     | TODO
+None.
 
 Results
 -------
 
 Results output by this step.
 
-| Result Key | Description
-|------------|------------
-| `TODO`     | TODO
+| Result Key  | Description
+|-------------|------------
+| `junit`     | A dictionary of JUnit unit test result dictionaries
 
 
-**Example**
+**junit**
+Keys in the `junit` dictionary element in the `unit-test` dictionary of the step results.
 
-    'tssc-results': {
-        'TODO': {
-            'TODO': ''
-        }
-    }
+| `junit` Key     | Description
+|-----------------|------------
+| `pom-path`      | Absolute path to the pom used to run tests
+| `test-results`  | Absolute path to the unit test results
 """
+import sys
+import os
+import sh
 
 from tssc import TSSCFactory
 from tssc import StepImplementer
 from tssc import DefaultSteps
+
+from xml.etree import ElementTree
+
+DEFAULT_CONFIG = {
+    'pom-file': 'pom.xml'
+}
+
+REQUIRED_CONFIG_KEYS = [
+    'pom-file'
+]
 
 class JUnit(StepImplementer):
     """
@@ -74,7 +85,7 @@ class JUnit(StepImplementer):
         dict
             Default values to use for step configuration values.
         """
-        return {}
+        return DEFAULT_CONFIG
 
     @staticmethod
     def required_runtime_step_config_keys():
@@ -90,7 +101,7 @@ class JUnit(StepImplementer):
         array_list
             Array of configuration keys that are required before running the step.
         """
-        return []
+        return REQUIRED_CONFIG_KEYS
 
     def _run_step(self, runtime_step_config):
         """
@@ -107,8 +118,60 @@ class JUnit(StepImplementer):
         dict
             Results of running this step.
         """
-        results = {}
+        pom_file = runtime_step_config['pom-file']
+
+        if not os.path.exists(pom_file):
+            raise ValueError('Given pom file does not exist: ' + pom_file)
+
+        reports_dir = self.find_reports_dir(pom_file)
+        if reports_dir is not None:
+            default_test_results_dir = reports_dir
+        else:
+            default_test_results_dir = os.path.join(
+                os.path.dirname(os.path.abspath(pom_file)),
+                'target/surefire-reports')
+
+        # TODO: Make method for identification/creation of this dir to be shared among steps
+        test_results_output_path = "tssc-results/unit-test/junit"
+
+        try:
+            sh.mvn(  # pylint: disable=no-member,
+                'clean',
+                'test',
+                '-f', pom_file,
+                _out=sys.stdout
+            )
+        except sh.ErrorReturnCode as error:
+            raise RuntimeError("Error invoking mvn: {error}".format(error=error))
+
+        os.system("mkdir -p " + test_results_output_path)
+        os.system("cp -r " + default_test_results_dir + "/. " + test_results_output_path)
+
+        # Consider adding specific results: Tests run: 3, Failures: 0, Errors: 0, Skipped: 0
+        results = {
+            'junit': {
+                'pom-path': pom_file,
+                'test-results': test_results_output_path
+            }
+        }
         return results
+    
+    @staticmethod
+    def find_reports_dir(pom_file):
+        """ Return the report directory specified in the pom """
+        # TODO: Figure out the pom namespace to make this work
+        ns = {'maven': 'http://maven.apache.org/POM/4.0.0'}
+        tree = ElementTree.parse(pom_file)
+        root = tree.getroot()
+
+        plugins = root.find('maven:build/maven:plugins', ns)
+        for plugin in plugins.findall('maven:plugin', ns):
+            config = plugin.find('maven:configuration', ns)
+            if config is not None:
+                reports_dir = config.find('maven:reportsDirectory', ns)
+                if reports_dir is not None:
+                    return reports_dir.text
+        return None
 
 # register step implementer
 TSSCFactory.register_step_implementer(JUnit)
