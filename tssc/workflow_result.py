2fc09805434ebc9187e1174a70d84a3dd2855c6e
Ashley Kim <ashleykim@localhost.localdomain>
2020-09-25 17:29:40 -0400

    Created static load and dump methods

--- tssc/workflow_result.py
+++ tssc/workflow_result.py
@@ -3,6 +3,7 @@ Abstract class and helper constants for WorkflowResult
 """
 import pickle
 import os
+import json
 
 from tssc.step_result import StepResult
 from tssc.exceptions import TSSCException
@@ -77,65 +78,85 @@ class WorkflowResult:
         for i in self.__workflow_list:
             print(i.get_step_result_yaml())
 
-    def dump(self, pickle_filename):
+    def dump_pickle(self, pickle_filename):
         """
         :return:
         """
         try:
-            _folder_create(pickle_filename)
+            WorkflowResult._folder_create(pickle_filename)
             with open(pickle_filename, 'wb') as file:
                 pickle.dump(self, file)
         except Exception as error:
             raise RuntimeError(f'error dumping {pickle_filename}: {error}') from error
+    
+    def dump_yml(self, yml_filename):
+        """
+        :return:
+        """
+        try:
+            WorkflowResult._folder_create(yml_filename)
+            with open(yml_filename, 'w') as file:
+                for i in self.__workflow_list:
+                    file.write(i.get_step_result_yaml())
+        except Exception as error:
+            raise RuntimeError(f'error dumping {yml_filename}: {error}') from error
 
+    def dump_json(self, json_filename):
+        """
+        :return:
+        """
+        try:
+            WorkflowResult._folder_create(json_filename)
+            with open(json_filename, 'w') as file:
+                for i in self.__workflow_list:
+                    file.write(i.get_step_result_json())
+        except Exception as error:
+            raise RuntimeError(f'error dumping {json_filename}: {error}') from error
 
-#######
-# helper methods
-#######
+    @staticmethod
+    def load(pickle_filename):
+        """
+        :return: Workflow_result object
+        """
+        try:
+            WorkflowResult._folder_create(pickle_filename)
 
-def _folder_create(pickle_filename):
-    """
-    :param filename:
-    :return:
-    """
-    path = os.path.dirname(pickle_filename)
-    if path and not os.path.exists(path):
-        os.makedirs(os.path.dirname(pickle_filename))
+            # if the file does not exist return empty object
+            if not os.path.isfile(pickle_filename):
+                return WorkflowResult()
 
+            # if the file is empty return empty object
+            if os.path.getsize(pickle_filename) == 0:
+                return WorkflowResult()
 
-def load_workflow_results(pickle_filename):
-    """
-    :return: Workflow_result object
-    """
-    try:
-        _folder_create(pickle_filename)
+            # check that the file has Workflow object
+            with open(pickle_filename, 'rb') as file:
+                workflow_result = pickle.load(file)
+                if not isinstance(workflow_result, WorkflowResult):
+                    raise RuntimeError(f'error {pickle_filename} has invalid data')
+                return workflow_result
 
-        # if the file does not exist return empty object
-        if not os.path.isfile(pickle_filename):
-            return WorkflowResult()
-
-        # if the file is empty return empty object
-        if os.path.getsize(pickle_filename) == 0:
-            return WorkflowResult()
-
-        # check that the file has Workflow object
-        with open(pickle_filename, 'rb') as file:
-            workflow_result = pickle.load(file)
-            if not isinstance(workflow_result, WorkflowResult):
-                raise RuntimeError(f'error {pickle_filename} has invalid data')
-            return workflow_result
-
-    except Exception as error:
-        raise RuntimeError(f'error loading {pickle_filename}: {error}') from error
-
-
-def delete_workflow_results(pickle_filename):
-    """
-    Currently used for testing - makes an empty file
-    """
-    try:
-        _folder_create(pickle_filename)
-        if os.path.exists(pickle_filename):
-            os.remove(pickle_filename)
-    except Exception as error:
-        raise RuntimeError(f'error deleting {pickle_filename}: {error}') from error
+        except Exception as error:
+            raise RuntimeError(f'error loading {pickle_filename}: {error}') from error
+    
+    @staticmethod
+    def _folder_create(filename):
+        """
+        :param filename:
+        :return:
+        """
+        path = os.path.dirname(filename)
+        if path and not os.path.exists(path):
+            os.makedirs(os.path.dirname(filename))
+    
+    @staticmethod
+    def delete(filename):
+        """
+        Currently used for testing - makes an empty file
+        """
+        try:
+            WorkflowResult._folder_create(filename)
+            if os.path.exists(filename):
+                os.remove(filename)
+        except Exception as error:
+            raise RuntimeError(f'error deleting {filename}: {error}') from error
