105d3813c53e8d03912538049609d2c72f9cfa72
matt <11786890+matthargrave@users.noreply.github.com>
2021-03-12 10:18:51 -0600

    creating helloworld

---
+++ src/ploigos_step_runner/generate_and_publish_workflow_report/helloworld.py
@@ -0,0 +1,44 @@
+"""`StepImplementers` for the `generate_and_publish_workflow_report` step.
+"""
+class HelloWorld(StepImplementer):  
+    """StepImplementer for the generate-and-publish-workflow-report step for HelloWorld.
+    """
+
+    @staticmethod
+    def step_implementer_config_defaults():
+        """Getter for the StepImplementer's configuration defaults.
+        Returns
+        -------
+        dict
+            Default values to use for step configuration values.
+        Notes
+        -----
+        These are the lowest precedence configuration values.
+        """
+        return DEFAULT_CONFIG
+
+    @staticmethod
+    def _required_config_or_result_keys():
+        """Getter for step configuration or previous step result artifacts that are required before
+        running this step.
+        See Also
+        --------
+        _validate_required_config_or_previous_step_result_artifact_keys
+        Returns
+        -------
+        array_list
+            Array of configuration keys or previous step result artifacts
+            that are required before running the step.
+        """
+        return REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS
+
+    def _run_step(self):
+        """Runs the step implemented by this StepImplementer.
+        Returns
+        -------
+        StepResult
+            Object containing the dictionary results of this step.
+        """
+        step_result = StepResult.from_step_implementer(self)
+	TODO - Add Logic Here
+        return step_result
