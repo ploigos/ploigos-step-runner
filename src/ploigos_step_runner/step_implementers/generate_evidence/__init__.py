30130720b0a330af480ad6086ea035ed9d22c345
John Thompkins <jthompki@redhat.com>
2021-06-25 12:09:54 -0600

    Fixed missing import statement for rekor in generate_evidence step

--- src/ploigos_step_runner/step_implementers/generate_evidence/__init__.py
+++ src/ploigos_step_runner/step_implementers/generate_evidence/__init__.py
@@ -3,3 +3,5 @@
 
 from ploigos_step_runner.step_implementers.generate_evidence.generate_evidence import \
     GenerateEvidence
+from ploigos_step_runner.step_implementers.generate_evidence.rekor_sign_evidence import \
+    RekorSignEvidence
