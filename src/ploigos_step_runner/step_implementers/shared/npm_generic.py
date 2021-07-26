3177d605465d35f420855d48ee42a7d80ea26e86
Kyle Rafaels <krafaels@redhat.com>
2021-07-26 12:29:57 -0500

    init commit of NPM step implementers. * TODO: fix unit tests * TODO: fix linting

---
+++ src/ploigos_step_runner/step_implementers/shared/npm_generic.py
@@ -0,0 +1,180 @@
+"""
+Step Configuration
+------------------
+Step configuration expected as input to this step.
+Could come from:
+* static configuration
+* runtime configuration
+* previous step results
+
+Configuration Key            | Required? | Default | Description
+-----------------------------|-----------|---------|-----------
+`package-file`               | No        | `'package.json'` | package.json file for reference app. 
+`npm-run-scripts`            | Yes       | `None`  | Script in package.json file to run. 
+"""
+
+
+import os
+from ploigos_step_runner import StepResult, StepRunnerException
+from ploigos_step_runner.step_implementer import StepImplementer
+from ploigos_step_runner.utils.npm import run_npm
+
+DEFAULT_CONFIG = {
+    'package-file': 'package.json'
+}
+
+REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS = [
+    'package-file',
+    'npm-run-scripts'
+]
+
+class NpmGeneric(StepImplementer):
+    """Abstract parent class for StepImplementers that use npm.
+    """
+
+    def __init__( # pylint: disable=too-many-arguments
+        self,
+        workflow_result,
+        parent_work_dir_path,
+        config,
+        environment=None,
+        npm_run_scripts=None
+    ):
+        self.__npm_run_scripts = npm_run_scripts
+
+        super().__init__(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            environment=environment
+        )
+
+    @staticmethod
+    def step_implementer_config_defaults():
+        """Getter for the StepImplementer's configuration defaults.
+
+        Returns
+        -------
+        dict
+            Default values to use for step configuration values.
+
+        Notes
+        -----
+        These are the lowest precedence configuration values.
+        """
+        return DEFAULT_CONFIG
+
+    @staticmethod
+    def _required_config_or_result_keys():
+        """Getter for step configuration or previous step result artifacts that are required before
+        running this step.
+
+        See Also
+        --------
+        _validate_required_config_or_previous_step_result_artifact_keys
+
+        Returns
+        -------
+        array_list
+            Array of configuration keys or previous step result artifacts
+            that are required before running the step.
+        """
+        return REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS
+
+    def _validate_required_config_or_previous_step_result_artifact_keys(self):
+        """Validates that the required configuration keys or previous step result artifacts
+        are set and have valid values.
+
+        Validates that:
+        * required configuration is given
+        * given 'package-file' exists
+
+        Raises
+        ------
+        AssertionError
+            If step configuration or previous step result artifacts have invalid required values
+        """
+        super()._validate_required_config_or_previous_step_result_artifact_keys()
+
+        # if package-file has value verify file exists
+        # If it doesn't have value and is required function will have already failed
+        package_file = self.get_value('package-file')
+        if package_file is not None:
+            assert os.path.exists(package_file), \
+                f'Given npm package file (package-file) does not exist: {package_file}'
+
+    @property
+    def npm_run_scripts(self):
+        """Property for getting the npm phases and goals to execute which can either come
+        from field set on this class via constructor, intended for use by sub classes that want
+        to hard code the phases and goals for convenience, or comes from config value
+        `npm-phases-and-goals` set by the user.
+
+        Returns
+        -------
+        str
+            Maven phases and/or goals to execute.
+        """
+        npm_run_scripts = None
+        if self.__npm_run_scripts:
+            npm_run_scripts = self.__npm_run_scripts
+        else:
+            npm_run_scripts = self.get_value('npm-run-scripts')
+
+        return npm_run_scripts
+
+    def _run_npm_step(
+        self,
+        npm_output_file_path
+    ):
+        """Runs npm using the configuration given to this step runner.
+
+        Parameters
+        ----------
+        npm_output_file_path : str
+            Path to file containing the npm stdout and stderr output.
+        step_implementer_additional_arguments : []
+            Additional arguments hard coded by the step implementer.
+
+        Raises
+        ------
+        StepRunnerException
+            If npm returns a none 0 exit code.
+        """
+
+        npm_run_scripts = self.npm_run_scripts
+
+        run_npm(
+            npm_output_file_path=npm_output_file_path,
+            npm_run_scripts=npm_run_scripts
+        )
+
+    def _run_step(self):
+        """Runs the step implemented by this StepImplementer.
+
+        Returns
+        -------
+        StepResult
+            Object containing the dictionary results of this step.
+        """
+        step_result = StepResult.from_step_implementer(self)
+
+        # package the artifacts
+        npm_output_file_path = self.write_working_file('npm_output.txt')
+        try:
+            # execute npm step (params come from config)
+            self._run_npm_step(
+                npm_output_file_path=npm_output_file_path
+            )
+        except StepRunnerException as error:
+            step_result.success = False
+            step_result.message = "Error running npm. " \
+                f"More details maybe found in 'npm-output' report artifact: {error}"
+        finally:
+            step_result.add_artifact(
+                description="Standard out and standard error from npm.",
+                name='npm-output',
+                value=npm_output_file_path
+            )
+
+        return step_result
---
+++ tests/step_implementers/shared/test_npm_generic.py
@@ -0,0 +1,369 @@
+
+import os
+from pathlib import Path
+# from shutil import copyfile
+from unittest.mock import PropertyMock, patch
+from ploigos_step_runner import StepResult, StepRunnerException, WorkflowResult
+from ploigos_step_runner.config.config import Config
+from ploigos_step_runner.step_implementers.shared.npm_generic import NpmGeneric
+# from ploigos_step_runner.utils.file import create_parent_dir
+from testfixtures import TempDirectory
+from tests.helpers.base_step_implementer_test_case import \
+    BaseStepImplementerTestCase
+
+
+@patch("ploigos_step_runner.StepImplementer.__init__")
+class TestStepImplementerSharedNpmGeneric___init__(BaseStepImplementerTestCase):
+
+    def test_no_environment_no_npm_run_scripts(self, mock_super_init):
+        workflow_result = WorkflowResult()
+        parent_work_dir_path = '/fake/path'
+        config = {}
+
+        step_implementer = NpmGeneric(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config
+        )
+
+        self.assertIsNone(step_implementer._NpmGeneric__npm_run_scripts)
+        mock_super_init.assert_called_once_with(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            environment=None
+        )
+
+    def test_with_environment_no_npm_run_scripts(self, mock_super_init):
+        workflow_result = WorkflowResult()
+        parent_work_dir_path = '/fake/path'
+        config = {}
+
+        step_implementer = NpmGeneric(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            environment='test-env'
+        )
+
+        self.assertIsNone(step_implementer._NpmGeneric__npm_run_scripts)
+        mock_super_init.assert_called_once_with(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            environment='test-env'
+        )
+
+    def test_no_environment_with_npm_run_scripts(self, mock_super_init):
+        workflow_result = WorkflowResult()
+        parent_work_dir_path = '/fake/path'
+        config = {}
+
+        step_implementer = NpmGeneric(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            npm_run_scripts=['fake-phase']
+        )
+
+        self.assertEqual(
+            step_implementer._NpmGeneric__npm_run_scripts,
+            ['fake-phase']
+        )
+        mock_super_init.assert_called_once_with(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            environment=None
+        )
+
+class TestStepImplementerSharedNpmGeneric_step_implementer_config_defaults(
+    BaseStepImplementerTestCase
+):
+    def test_result(self):
+        self.assertEqual(
+            NpmGeneric.step_implementer_config_defaults(),
+            {
+                'package-file': 'package.json',
+            }
+        )
+
+class TestStepImplementerSharedNpmGeneric__required_config_or_result_keys(
+    BaseStepImplementerTestCase
+):
+    def test_result(self):
+        self.assertEqual(
+            NpmGeneric._required_config_or_result_keys(),
+            [
+                'package-file',
+                'npm-run-scripts'
+            ]
+        )
+
+class BaseTestStepImplementerSharedNpmGeneric(BaseStepImplementerTestCase):
+    def create_step_implementer(
+            self,
+            step_config={},
+            workflow_result=None,
+            parent_work_dir_path=''
+    ):
+        return self.create_given_step_implementer(
+            step_implementer=NpmGeneric,
+            step_config=step_config,
+            step_name='foo',
+            implementer='NpmGeneric',
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path
+        )
+
+@patch("ploigos_step_runner.StepImplementer._validate_required_config_or_previous_step_result_artifact_keys")
+class TestStepImplementerSharedNpmGeneric__validate_required_config_or_previous_step_result_artifact_keys(
+    BaseTestStepImplementerSharedNpmGeneric
+):
+    def test_valid(self, mock_super_validate):
+        with TempDirectory() as test_dir:
+            parent_work_dir_path = os.path.join(test_dir.path, 'working')
+
+            package_file_path = os.path.join(test_dir.path, 'package.json')
+            step_config = {
+                'package-file': package_file_path,
+                'npm-run-scripts': ['fake-script']
+            }
+
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            Path(package_file_path).touch()
+            step_implementer._validate_required_config_or_previous_step_result_artifact_keys()
+
+            mock_super_validate.assert_called_once_with()
+
+    def test_package_file_does_not_exist(self, mock_super_validate):
+        with TempDirectory() as test_dir:
+            parent_work_dir_path = os.path.join(test_dir.path, 'working')
+
+            package_file_path = '/does/not/exist/package.json'
+            step_config = {
+                'package-file': package_file_path,
+                'npm-run-scripts': 'fake-phase'
+            }
+
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            with self.assertRaisesRegex(
+                AssertionError,
+                rf'Given npm package file \(package-file\) does not exist: {package_file_path}'
+            ):
+                step_implementer._validate_required_config_or_previous_step_result_artifact_keys()
+                mock_super_validate.assert_called_once_with()
+
+class TestStepImplementerSharedNpmGeneric_maven_phases_and_goals(
+    BaseTestStepImplementerSharedNpmGeneric
+):
+    def test_use_object_property_no_config_value(self):
+        workflow_result = WorkflowResult()
+        parent_work_dir_path = '/fake/path'
+        config = None
+
+        step_implementer = NpmGeneric(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            npm_run_scripts=['fake-phase']
+        )
+
+        self.assertEqual(
+            step_implementer.npm_run_scripts,
+            ['fake-phase']
+        )
+
+    def test_use_object_property_with_config_value(self):
+        workflow_result = WorkflowResult()
+        parent_work_dir_path = '/fake/path'
+        step_config = {
+            'npm-run-scripts': ['config-value-fake-phase']
+        }
+        config = Config({
+            Config.CONFIG_KEY: {
+                'foo': [
+                    {
+                        'implementer': 'NpmGeneric',
+                        'config': step_config
+                    }
+                ]
+
+            }
+        })
+
+        step_implementer = NpmGeneric(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            npm_run_scripts=['object-property-fake-phase']
+        )
+
+        self.assertEqual(
+            step_implementer.npm_run_scripts,
+            ['object-property-fake-phase']
+        )
+
+    def test_use_config_value(self):
+        parent_work_dir_path = '/fake/path'
+        step_config = {
+            'npm-run-scripts': ['config-value-fake-script']
+        }
+
+        step_implementer = self.create_step_implementer(
+            step_config=step_config,
+            parent_work_dir_path=parent_work_dir_path,
+        )
+
+        self.assertEqual(
+            step_implementer.npm_run_scripts,
+            ['config-value-fake-script']
+        )
+
+@patch('ploigos_step_runner.step_implementers.shared.npm_generic.run_npm')
+@patch.object(
+    NpmGeneric,
+    'npm_run_scripts',
+    new_callable=PropertyMock,
+    return_value=['fake-phase']
+)
+
+class TestStepImplementerSharedNpmGeneric__run_npm_step(BaseTestStepImplementerSharedNpmGeneric):
+    def test_defaults(self, mock_run_scripts, mock_run_npm):
+        with TempDirectory() as test_dir:
+            parent_work_dir_path = os.path.join(test_dir.path, 'working')
+
+            package_file_path = os.path.join(test_dir.path, 'package.json')
+            step_config = {
+                'package-file': package_file_path
+            }
+
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            npm_output_file_path = os.path.join(test_dir.path, 'npm-output.txt')
+            step_implementer._run_npm_step(
+                npm_output_file_path=npm_output_file_path
+            )
+
+            mock_run_npm.assert_called_with(
+                npm_output_file_path=npm_output_file_path,
+                npm_run_scripts=['fake-phase']
+                )
+
+    # def test_yes_transfer_progress(self, mock_run_scripts, mock_run_npm):
+    #     with TempDirectory() as test_dir:
+    #         parent_work_dir_path = os.path.join(test_dir.path, 'working')
+
+    #         package_file_path = os.path.join(test_dir.path, 'package.json')
+    #         step_config = {
+    #             'package-file': package_file_path,
+    #             'npm-no-transfer-progress': False
+    #         }
+
+    #         step_implementer = self.create_step_implementer(
+    #             step_config=step_config,
+    #             parent_work_dir_path=parent_work_dir_path,
+    #         )
+
+    #         npm_output_file_path = os.path.join(test_dir.path, 'npm-output.txt')
+    #         step_implementer._run_npm_step(
+    #             npm_output_file_path=npm_output_file_path
+    #         )
+
+    #         mock_run_npm.assert_called_with(
+    #             npm_output_file_path=npm_output_file_path,
+    #             run_scripts=['fake-phase'],
+    #             package_file=package_file_path,
+    #             no_transfer_progress=False
+    #         )
+
+@patch.object(NpmGeneric, '_run_npm_step')
+@patch.object(NpmGeneric, 'write_working_file', return_value='/mock/npm_output.txt')
+class TestStepImplementerSharedNpmGeneric__run_step(
+    BaseTestStepImplementerSharedNpmGeneric):
+    def test_success(self, mock_write_working_file, mock_run_npm_step):
+        with TempDirectory() as test_dir:
+            parent_work_dir_path = os.path.join(test_dir.path, 'working')
+
+            step_config = {}
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            # run step
+            actual_step_result = step_implementer._run_step()
+
+            # create expected step result
+            expected_step_result = StepResult(
+                step_name='foo',
+                sub_step_name='NpmGeneric',
+                sub_step_implementer_name='NpmGeneric'
+            )
+            expected_step_result.add_artifact(
+                description="Standard out and standard error from npm.",
+                name='npm-output',
+                value='/mock/npm_output.txt'
+            )
+
+            # verify step result
+            self.assertEqual(
+                actual_step_result,
+                expected_step_result
+            )
+
+            mock_run_npm_step.assert_called_with(
+                npm_output_file_path='/mock/npm_output.txt'
+            )
+
+    def test_fail(self, mock_write_working_file, mock_run_npm_step):
+        with TempDirectory() as test_dir:
+            parent_work_dir_path = os.path.join(test_dir.path, 'working')
+
+            step_config = {}
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            # run step with mock failure
+            mock_run_npm_step.side_effect = StepRunnerException('Mock error running npm')
+            actual_step_result = step_implementer._run_step()
+
+            # create expected step result
+            expected_step_result = StepResult(
+                step_name='foo',
+                sub_step_name='NpmGeneric',
+                sub_step_implementer_name='NpmGeneric'
+            )
+            expected_step_result.add_artifact(
+                description="Standard out and standard error from npm.",
+                name='npm-output',
+                value='/mock/npm_output.txt'
+            )
+            expected_step_result.message = "Error running npm. " \
+                "More details maybe found in 'npm-output' report artifact: "\
+                "Mock error running npm"
+            expected_step_result.success = False
+
+            # verify step result
+            self.assertEqual(
+                actual_step_result,
+                expected_step_result
+            )
+
+            mock_write_working_file.assert_called_once()
+            mock_run_npm_step.assert_called_with(
+                npm_output_file_path='/mock/npm_output.txt'
+            )
