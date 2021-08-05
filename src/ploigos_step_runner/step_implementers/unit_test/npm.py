bd8106db8fa4d3ed355cca92372db51065907889
Kyle Rafaels <krafaels@redhat.com>
2021-08-05 14:47:08 -0500

    using npmgeneric class

--- src/ploigos_step_runner/step_implementers/unit_test/npm.py
+++ src/ploigos_step_runner/step_implementers/unit_test/npm.py
@@ -4,7 +4,8 @@
 import os
 from posixpath import abspath
 import sys
-from ploigos_step_runner.step_implementer import StepImplementer
+# from ploigos_step_runner.step_implementer import StepImplementer
+from ploigos_step_runner.step_implementers.shared.npm_generic import NpmGeneric
 from ploigos_step_runner.utils.io import create_sh_redirect_to_multiple_streams_fn_callback
 
 import sh
@@ -17,9 +18,12 @@ DEFAULT_CONFIG = {
 REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS = [
     'package-file'
 ]
-class Npm(StepImplementer):
+class Npm(NpmGeneric):
     """`StepImplementer` for the `unit-test` step using npm.
     """
+
+    def __init__(self, workflow_result, parent_work_dir_path, config, environment, npm_targets):
+        super().__init__(workflow_result, parent_work_dir_path, config, environment=environment, npm_targets=['test'])
 
     @staticmethod
     def step_implementer_config_defaults():
@@ -34,7 +38,7 @@ class Npm(StepImplementer):
         dict
             Default values to use for step configuration values.
         """
-        return DEFAULT_CONFIG
+        return {**NpmGeneric.step_implementer_config_defaults(), **DEFAULT_CONFIG}
 
     @staticmethod
     def _required_config_or_result_keys():
@@ -53,24 +57,24 @@ class Npm(StepImplementer):
         """
         return REQUIRED_CONFIG_OR_PREVIOUS_STEP_RESULT_ARTIFACT_KEYS
 
-    def _validate_required_config_or_previous_step_result_artifact_keys(self):
-        """Validates that the required configuration keys or previous step result artifacts
-        are set and have valid values.
+    # def _validate_required_config_or_previous_step_result_artifact_keys(self):
+    #     """Validates that the required configuration keys or previous step result artifacts
+    #     are set and have valid values.
 
-        Validates that:
-        * required configuration is given
-        * given 'package-file' exists
+    #     Validates that:
+    #     * required configuration is given
+    #     * given 'package-file' exists
 
-        Raises
-        ------
-        AssertionError
-            If step configuration or previous step result artifacts have invalid required values
-        """
-        super()._validate_required_config_or_previous_step_result_artifact_keys()
+    #     Raises
+    #     ------
+    #     AssertionError
+    #         If step configuration or previous step result artifacts have invalid required values
+    #     """
+    #     super()._validate_required_config_or_previous_step_result_artifact_keys()
 
-        package_file = self.get_value('package-file')
-        assert os.path.exists(package_file), \
-            f'Given npm package file (package-file) does not exist: {package_file}'
+    #     package_file = self.get_value('package-file')
+    #     assert os.path.exists(package_file), \
+    #         f'Given npm package file (package-file) does not exist: {package_file}'
 
     def _run_step(self):
         """Runs the step implemented by this StepImplementer.
@@ -84,22 +88,9 @@ class Npm(StepImplementer):
 
         npm_output_file_path = self.write_working_file('npm_test_output.txt')
         try:
-            with open(npm_output_file_path, 'w') as npm_output_file:
-                out_callback = create_sh_redirect_to_multiple_streams_fn_callback([
-                    sys.stdout,
-                    npm_output_file
-                ])
-                err_callback = create_sh_redirect_to_multiple_streams_fn_callback([
-                    sys.stderr,
-                    npm_output_file
-                ])
-
-                sh.npm( # pylint: disable=no-member
-                    'run',
-                    'test',
-                    _out=out_callback,
-                    _err=err_callback
-                )
+            self._run_npm_step(
+                npm_output_file_path=npm_output_file_path
+            )
 
         except sh.ErrorReturnCode as error:
             step_result.message = "Unit test failures. See 'npm-output'" \
