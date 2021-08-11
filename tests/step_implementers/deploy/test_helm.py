7f61dd25fa7d7f15c6647ce17c69d5fa0598b8c4
Gerard Fulton <gfulton@redhat.com>
2021-08-10 17:27:30 -0700

    add base test

--- tests/step_implementers/deploy/test_helm.py
+++ tests/step_implementers/deploy/test_helm.py
@@ -9,31 +9,28 @@ from tests.helpers.test_utils import Any, create_sh_side_effect
 from ploigos_step_runner.step_implementers.deploy.helm import Helm
 
 
-# @patch("ploigos_step_runner.step_implementers.shared.NpmGeneric.__init__")
-# class TestStepImplementerMavenTest___init__(BaseStepImplementerTestCase):
-#     def test_defaults(self, mock_super_init):
-#         workflow_result = WorkflowResult()
-#         parent_work_dir_path = '/fake/path'
-#         config = {}
-#
-#         Helm(
-#             workflow_result=workflow_result,
-#             parent_work_dir_path=parent_work_dir_path,
-#             config=config,
-#             environment=None,
-#             helm_chart=None,
-#             helm_release=None
-#         )
-#
-#         mock_super_init.assert_called_once_with(
-#             workflow_result,
-#             parent_work_dir_path,
-#             config,
-#             environment=None,
-#             helm_chart=None,
-#             helm_release=None,
-#             helm_flags=[]
-#         )
+@patch("ploigos_step_runner.step_implementers.deploy.Helm.__init__")
+class TestStepImplementerMavenTest___init__(BaseStepImplementerTestCase):
+    def test_defaults(self, mock_super_init):
+        workflow_result = WorkflowResult()
+        parent_work_dir_path = '/fake/path'
+        config = {}
+
+        Helm(
+            workflow_result=workflow_result,
+            parent_work_dir_path=parent_work_dir_path,
+            config=config,
+            environment=None,
+            helm_chart=None,
+            helm_release=None
+        )
+
+        mock_super_init.assert_called_once_with(
+            workflow_result,
+            parent_work_dir_path,
+            config,
+            environment=None
+        )
 
 class TestStepImplementerDeployHelmBase(BaseStepImplementerTestCase):
     def create_step_implementer(
