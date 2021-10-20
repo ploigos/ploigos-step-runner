55d32f6fb16e09ef5ce60a9a7d9b72905b72bbba
Christopher May <chmay@redhat.com>
2021-10-20 15:47:12 -0400

    Add unit tests for 'increment version' happy and negative paths

--- tests/step_implementers/generate_metadata/test_maven_generate_metadata.py
+++ tests/step_implementers/generate_metadata/test_maven_generate_metadata.py
@@ -2,8 +2,7 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 import os
-from unittest.mock import patch
-from re import escape
+from unittest.mock import patch, call, PropertyMock
 
 from testfixtures import TempDirectory
 from tests.helpers.base_step_implementer_test_case import \
@@ -156,6 +155,435 @@ class TestStepImplementerMavenGenerateMetadata(BaseStepImplementerTestCase):
                 sub_step_implementer_name='Maven'
             )
             expected_step_result.add_artifact(name='app-version', value='42.1')
+
+            self.assertEqual(result, expected_step_result)
+
+    @patch.object(
+        Maven,
+        'maven_settings_file',
+        new_callable=PropertyMock,
+        return_value='/fake/settings.xml'
+    )
+    @patch('ploigos_step_runner.step_implementers.generate_metadata.maven.run_maven')
+    def test_run_step_pass_increment_major_version(
+            self,
+            mock_run_maven,
+            mock_maven_settings
+    ):
+        with TempDirectory() as temp_dir:
+            # Test setup
+            parent_work_dir_path = os.path.join(temp_dir.path, 'working')
+
+            temp_dir.write('pom.xml', b'''<project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>41.1.12</version>
+            </project>''')
+            pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
+
+            step_config = {
+                'pom-file': pom_file_path,
+                'auto-increment-version-segment': 'major'
+            }
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                step_name='generate-metadata',
+                implementer='Maven',
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            mock_run_maven.return_value = '42.0.0'
+
+            # Invoke method under test
+            result = step_implementer._run_step()
+
+            # Assertions / Validations
+            mock_run_maven.assert_has_calls([
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'build-helper:parse-version',
+                        'versions:set',
+                        'versions:commit'
+                    ],
+                    additional_arguments=[
+                        r'-DnewVersion=${parsedVersion.nextMajorVersion}.0.0',
+                        '-DprocessAllModules'
+                    ]
+                ),
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_evaluate_project_version.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'help:evaluate'
+                    ],
+                    additional_arguments=[
+                        '-Dexpression=project.version',
+                        '--batch-mode',
+                        '-q',
+                        '-DforceStdout'
+                    ]
+                )
+            ])
+
+            expected_step_result = StepResult(
+                step_name='generate-metadata',
+                sub_step_name='Maven',
+                sub_step_implementer_name='Maven'
+            )
+            expected_step_result.add_artifact(
+                description="Standard out and standard error from running maven"
+                            " to auto increment version.",
+                name='maven-auto-increment-version-output',
+                value=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt'
+            )
+            expected_step_result.add_artifact(name='app-version', value='42.0.0')
+
+            self.assertEqual(result, expected_step_result)
+
+    @patch.object(
+        Maven,
+        'maven_settings_file',
+        new_callable=PropertyMock,
+        return_value='/fake/settings.xml'
+    )
+    @patch('ploigos_step_runner.step_implementers.generate_metadata.maven.run_maven')
+    def test_run_step_pass_increment_minor_version(
+            self,
+            mock_run_maven,
+            mock_maven_settings
+    ):
+        with TempDirectory() as temp_dir:
+            # Test setup
+            parent_work_dir_path = os.path.join(temp_dir.path, 'working')
+
+            temp_dir.write('pom.xml', b'''<project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>41.1.12</version>
+            </project>''')
+            pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
+
+            step_config = {
+                'pom-file': pom_file_path,
+                'auto-increment-version-segment': 'minor'
+            }
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                step_name='generate-metadata',
+                implementer='Maven',
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            mock_run_maven.return_value = '41.2.0'
+
+            # Invoke method under test
+            result = step_implementer._run_step()
+
+            # Assertions / Validations
+            mock_run_maven.assert_has_calls([
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'build-helper:parse-version',
+                        'versions:set',
+                        'versions:commit'
+                    ],
+                    additional_arguments=[
+                        r'-DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.nextMinorVersion}.0',
+                        '-DprocessAllModules'
+                    ]
+                ),
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_evaluate_project_version.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'help:evaluate'
+                    ],
+                    additional_arguments=[
+                        '-Dexpression=project.version',
+                        '--batch-mode',
+                        '-q',
+                        '-DforceStdout'
+                    ]
+                )
+            ])
+
+            expected_step_result = StepResult(
+                step_name='generate-metadata',
+                sub_step_name='Maven',
+                sub_step_implementer_name='Maven'
+            )
+            expected_step_result.add_artifact(
+                description="Standard out and standard error from running maven"
+                            " to auto increment version.",
+                name='maven-auto-increment-version-output',
+                value=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt'
+            )
+            expected_step_result.add_artifact(name='app-version', value='41.2.0')
+
+            self.assertEqual(result, expected_step_result)
+
+    @patch.object(
+        Maven,
+        'maven_settings_file',
+        new_callable=PropertyMock,
+        return_value='/fake/settings.xml'
+    )
+    @patch('ploigos_step_runner.step_implementers.generate_metadata.maven.run_maven')
+    def test_run_step_pass_increment_patch_version(
+            self,
+            mock_run_maven,
+            mock_maven_settings
+    ):
+        with TempDirectory() as temp_dir:
+            # Test setup
+            parent_work_dir_path = os.path.join(temp_dir.path, 'working')
+
+            temp_dir.write('pom.xml', b'''<project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>41.1.12</version>
+            </project>''')
+            pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
+
+            step_config = {
+                'pom-file': pom_file_path,
+                'auto-increment-version-segment': 'patch'
+            }
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                step_name='generate-metadata',
+                implementer='Maven',
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            mock_run_maven.return_value = '41.1.13'
+
+            # Invoke method under test
+            result = step_implementer._run_step()
+
+            # Assertions / Validations
+            mock_run_maven.assert_has_calls([
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'build-helper:parse-version',
+                        'versions:set',
+                        'versions:commit'
+                    ],
+                    additional_arguments=[
+                        r'-DnewVersion=${parsedVersion.majorVersion}'
+                        r'.${parsedVersion.minorVersion}'
+                        r'.${parsedVersion.nextIncrementalVersion}',
+                        '-DprocessAllModules'
+                    ]
+                ),
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_evaluate_project_version.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'help:evaluate'
+                    ],
+                    additional_arguments=[
+                        '-Dexpression=project.version',
+                        '--batch-mode',
+                        '-q',
+                        '-DforceStdout'
+                    ]
+                )
+            ])
+
+            expected_step_result = StepResult(
+                step_name='generate-metadata',
+                sub_step_name='Maven',
+                sub_step_implementer_name='Maven'
+            )
+            expected_step_result.add_artifact(
+                description="Standard out and standard error from running maven"
+                            " to auto increment version.",
+                name='maven-auto-increment-version-output',
+                value=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt'
+            )
+            expected_step_result.add_artifact(name='app-version', value='41.1.13')
+
+            self.assertEqual(result, expected_step_result)
+
+    @patch.object(
+        Maven,
+        'maven_settings_file',
+        new_callable=PropertyMock,
+        return_value='/fake/settings.xml'
+    )
+    @patch('ploigos_step_runner.step_implementers.generate_metadata.maven.run_maven')
+    def test_run_step_pass_increment_patch_version_do_not_increment_all_modules(
+            self,
+            mock_run_maven,
+            mock_maven_settings
+    ):
+        with TempDirectory() as temp_dir:
+            # Test setup
+            parent_work_dir_path = os.path.join(temp_dir.path, 'working')
+
+            temp_dir.write('pom.xml', b'''<project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>41.1.12</version>
+            </project>''')
+            pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
+
+            step_config = {
+                'pom-file': pom_file_path,
+                'auto-increment-version-segment': 'patch',
+                'auto-increment-all-module-versions': False
+            }
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                step_name='generate-metadata',
+                implementer='Maven',
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            mock_run_maven.return_value = '41.1.13'
+
+            # Invoke method under test
+            result = step_implementer._run_step()
+
+            # Assertions / Validations
+            mock_run_maven.assert_has_calls([
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'build-helper:parse-version',
+                        'versions:set',
+                        'versions:commit'
+                    ],
+                    additional_arguments=[
+                        r'-DnewVersion=${parsedVersion.majorVersion}'
+                        r'.${parsedVersion.minorVersion}'
+                        r'.${parsedVersion.nextIncrementalVersion}'
+                    ]
+                ),
+                call(
+                    mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_evaluate_project_version.txt',
+                    settings_file='/fake/settings.xml',
+                    pom_file=pom_file_path,
+                    phases_and_goals=[
+                        'help:evaluate'
+                    ],
+                    additional_arguments=[
+                        '-Dexpression=project.version',
+                        '--batch-mode',
+                        '-q',
+                        '-DforceStdout'
+                    ]
+                )
+            ])
+
+            expected_step_result = StepResult(
+                step_name='generate-metadata',
+                sub_step_name='Maven',
+                sub_step_implementer_name='Maven'
+            )
+            expected_step_result.add_artifact(
+                description="Standard out and standard error from running maven"
+                            " to auto increment version.",
+                name='maven-auto-increment-version-output',
+                value=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt'
+            )
+            expected_step_result.add_artifact(name='app-version', value='41.1.13')
+
+            self.assertEqual(result, expected_step_result)
+
+    @patch.object(
+        Maven,
+        'maven_settings_file',
+        new_callable=PropertyMock,
+        return_value='/fake/settings.xml'
+    )
+    @patch('ploigos_step_runner.step_implementers.generate_metadata.maven.run_maven')
+    def test_run_step_fail_increment_version(
+            self,
+            mock_run_maven,
+            mock_maven_settings
+    ):
+        with TempDirectory() as temp_dir:
+            # Test setup
+            parent_work_dir_path = os.path.join(temp_dir.path, 'working')
+
+            temp_dir.write('pom.xml', b'''<project>
+                <modelVersion>4.0.0</modelVersion>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>my-app</artifactId>
+                <version>41.1.12</version>
+            </project>''')
+            pom_file_path = os.path.join(temp_dir.path, 'pom.xml')
+
+            step_config = {
+                'pom-file': pom_file_path,
+                'auto-increment-version-segment': 'patch'
+            }
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                step_name='generate-metadata',
+                implementer='Maven',
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            mock_run_maven.side_effect = StepRunnerException('Whoops!')
+
+            # Invoke method under test
+            result = step_implementer._run_step()
+
+            # Assertions / Validations
+            mock_run_maven.assert_called_once_with(
+                mvn_output_file_path=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt',
+                settings_file='/fake/settings.xml',
+                pom_file=pom_file_path,
+                phases_and_goals=[
+                    'build-helper:parse-version',
+                    'versions:set',
+                    'versions:commit'
+                ],
+                additional_arguments=[
+                    r'-DnewVersion=${parsedVersion.majorVersion}'
+                    r'.${parsedVersion.minorVersion}'
+                    r'.${parsedVersion.nextIncrementalVersion}',
+                    '-DprocessAllModules'
+                ]
+            )
+
+            expected_step_result = StepResult(
+                step_name='generate-metadata',
+                sub_step_name='Maven',
+                sub_step_implementer_name='Maven'
+            )
+            expected_step_result.success = False
+            expected_step_result.message = f"Error running maven to auto increment version segment" \
+                f" (patch)." \
+                f" More details maybe found in 'maven-auto-increment-version-output'" \
+                f" report artifact: Whoops!"
+            expected_step_result.add_artifact(
+                description="Standard out and standard error from running maven"
+                            " to auto increment version.",
+                name='maven-auto-increment-version-output',
+                value=f'{parent_work_dir_path}/generate-metadata/mvn_versions_set_output.txt'
+            )
 
             self.assertEqual(result, expected_step_result)
 
