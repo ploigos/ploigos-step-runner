07d81b42dcf281bd9a5e65a910bf772c1257d13c
Dillon Winchell <dwinchel@redhat.com>
2021-11-23 13:33:25 -0500

    Add support for npm push-artifacts step.

--- src/ploigos_step_runner/step_implementers/push_artifacts/__init__.py
+++ src/ploigos_step_runner/step_implementers/push_artifacts/__init__.py
@@ -3,3 +3,4 @@
 
 from ploigos_step_runner.step_implementers.push_artifacts.maven_deploy import MavenDeploy
 from ploigos_step_runner.step_implementers.push_artifacts.maven import Maven
+from ploigos_step_runner.step_implementers.push_artifacts.npm_push_artifacts import NpmPushArtifacts
