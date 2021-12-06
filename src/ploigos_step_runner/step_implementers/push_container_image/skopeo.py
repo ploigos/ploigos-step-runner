631036a51a092da6b3c54489ca9858676bb7e627
Christopher Mason <cmason@redhat.com>
2021-12-06 16:33:30 -0500

    Chris Mason: Add container auth file to get digest call for secured registires.

--- src/ploigos_step_runner/step_implementers/push_container_image/skopeo.py
+++ src/ploigos_step_runner/step_implementers/push_container_image/skopeo.py
@@ -231,7 +231,8 @@ class Skopeo(StepImplementer):
             try:
                 print("Get pushed container image digest")
                 container_image_digest = get_container_image_digest(
-                    container_image_address=container_image_push_address_by_tag
+                    container_image_address=container_image_push_address_by_tag,
+                    containers_config_auth_file=containers_config_auth_file
                 )
 
                 container_image_short_address_by_digest = \
