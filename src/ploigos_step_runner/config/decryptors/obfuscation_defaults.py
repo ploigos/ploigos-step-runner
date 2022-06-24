1e34eae8e28377f36ea358dcb7c1bfa92dbeeb3f
Dillon Winchell <dwinchel@redhat.com>
2022-06-24 15:31:43 -0400

    Fixed linting error

--- src/ploigos_step_runner/config/decryptors/obfuscation_defaults.py
+++ src/ploigos_step_runner/config/decryptors/obfuscation_defaults.py
@@ -1,24 +1,23 @@
-"""ConfigValueDecryptor that does not really decrypt anything.
-It just flags ConfigValues that have potentially sensitivenames
-(for example they contain "password" and "username"), so that they are obfuscated
-in the PSR output ... just in case
+"""ConfigValueDecryptor that does not really decrypt anything, but flags values that should be
+obfuscated (like "password" and "username").
+
+Parameters
+----------
+None
 """
+
 import re
 from ploigos_step_runner.config.config_value_decryptor import ConfigValueDecryptor
 
-
-
-
 class ObfuscationDefaults(ConfigValueDecryptor):
-    """ConfigValueDecryptor that does not really decrypt anything.
-It just flags ConfigValues that have potentially sensitivenames
-(for example they contain "password" and "username"), so that they are obfuscated
-in the PSR output ... just in case.
+    """ConfigValueDecryptor that does not really decrypt anything, but flags values that should be
+    obfuscated (like "password" and "username").
 
     Parameters
     ----------
-
+    None
     """
+
     def can_decrypt(self, config_value):
         for path_part in config_value.path_parts:
             if re.search(".*(password|username).*", str(path_part)):
