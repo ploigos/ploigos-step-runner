be5e77a9f33d7dcb168a4e5d7a9058724185e157
Dillon Winchell <dwinchel@redhat.com>
2022-08-18 15:32:58 -0400

    Allow insecure tls for integration test cluster login

--- tests/integration/run-integration-tests.sh
+++ tests/integration/run-integration-tests.sh
@@ -19,7 +19,7 @@ cp ${SCRIPT_DIR}/everything-pipelinerun-template.yml everything-pipelinerun.yml
 sed -i "s,STEP_RUNNER_LIB_SOURCE_URL,${STEP_RUNNER_LIB_SOURCE_URL}," everything-pipelinerun.yml
 
 # Create the PipelineRun
-oc login --server=${OPENSHIFT_URL} -u ${OPENSHIFT_USER} -p ${OPENSHIFT_PASSWORD}
+oc login --insecure-skip-tls-verify --server=${OPENSHIFT_URL} -u ${OPENSHIFT_USER} -p ${OPENSHIFT_PASSWORD}
 CREATED_PIPELINERUN=$(oc create -f everything-pipelinerun.yml -n ${PIPELINE_NAMESPACE} | cut -d ' ' -f 1)
 
 # Wait for the pipeline to finish
