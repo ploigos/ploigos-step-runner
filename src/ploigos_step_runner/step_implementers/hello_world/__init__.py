55918e8c10112db18cff382a468f012ae470a3d3
Ryan Miller <miller@redhat.com>
2022-09-06 23:02:29 -0400

    Add LongGreeting implementer for hello-world

--- src/ploigos_step_runner/step_implementers/hello_world/__init__.py
+++ src/ploigos_step_runner/step_implementers/hello_world/__init__.py
@@ -1,4 +1,5 @@
 """`StepImplementers` for the `hello-world` step.
 """
 
+from ploigos_step_runner.step_implementers.hello_world.long_greeting import LongGreeting
 from ploigos_step_runner.step_implementers.hello_world.short_greeting import ShortGreeting
