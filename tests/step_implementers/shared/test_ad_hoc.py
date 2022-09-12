bbaf9cb8029738bd919143d8fd592c13a634dbb0
Ryan Miller <miller@redhat.com>
2022-09-12 19:43:17 -0400

    Test that artifact is added with AdHoc output

--- tests/step_implementers/shared/test_ad_hoc.py
+++ tests/step_implementers/shared/test_ad_hoc.py
@@ -70,3 +70,33 @@ class TestAdHocPackage__run_step(BaseStepImplementerTestCase):
 
             # Then the StepResult should have success = False
             self.assertEqual(step_result.success, False)
+
+    # Given a shell command, 'bash'
+    @patch('sh.bash', create=True)
+    def test_add_artifact_with_command_output(self, bash_shell_command_mock):
+
+        # Given a working directory
+        with TempDirectory() as temp_dir:
+
+            # Given an AdHoc step implementer
+            working_dir_path = os.path.join(temp_dir.path, 'working')
+            config = {
+                'command': 'echo "Hello World!"'
+            }
+            ad_hoc_test = self.create_given_step_implementer(
+                AdHoc,
+                parent_work_dir_path=working_dir_path,
+                step_config=config
+            )
+
+            # When I run the step
+            step_result = ad_hoc_test.run_step()
+
+            # Then the StepResult should have an artifact with the npm output:
+            artifact = step_result.get_artifact('command-output')
+            output_file_path = os.path.join(working_dir_path, 'ad_hoc_output.txt')
+            self.assertEqual(artifact, StepResultArtifact(
+                name='command-output',
+                value=output_file_path,
+                description="Standard out and standard error from ad-hoc command run."
+            ))
