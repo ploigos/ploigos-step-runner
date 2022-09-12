99d328708210efd12690bea6b5e497e4677d3812
Ryan Miller <miller@redhat.com>
2022-09-12 19:38:44 -0400

    Add test to validate AdHoc returns success false

--- tests/step_implementers/shared/test_ad_hoc.py
+++ tests/step_implementers/shared/test_ad_hoc.py
@@ -1,10 +1,11 @@
 import os
 from io import StringIO
 from unittest.mock import patch
+from ploigos_step_runner import config
 
 import sh
 from ploigos_step_runner.results import StepResultArtifact
-from ploigos_step_runner.step_implementers.shared import AdHoc
+from ploigos_step_runner.step_implementers.shared import AdHoc, ad_hoc
 from testfixtures import TempDirectory
 from tests.helpers.base_step_implementer_test_case import \
     BaseStepImplementerTestCase
@@ -12,7 +13,7 @@ from tests.helpers.test_utils import Any
 
 
 class TestAdHocPackage__run_step(BaseStepImplementerTestCase):
-    # Given a shell command, 'npm'
+    # Given a shell command, 'bash'
     @patch('sh.bash', create=True)
     def test_run_shell_command(self, bash_shell_command_mock):
 
@@ -21,10 +22,10 @@ class TestAdHocPackage__run_step(BaseStepImplementerTestCase):
             working_dir_path = os.path.join(temp_dir.path, 'working')
 
             config = {
-                'command': 'echo Hello World!'
+                'command': 'echo "Hello World!"'
             }
 
-            # Given an NpmPackage step implementer
+            # Given an AdHoc step implementer
             ad_hoc_test = self.create_given_step_implementer(
                 AdHoc,
                 step_config=config,
@@ -34,10 +35,38 @@ class TestAdHocPackage__run_step(BaseStepImplementerTestCase):
             # When I run the step
             ad_hoc_test.run_step()
 
-            # Then it should run a shell command, 'npm test'
+            # Then it should run a shell command, 'echo "Hello World!"'
             bash_shell_command_mock.assert_any_call(
                 '-c',
-                'echo Hello World!',
+                'echo "Hello World!"',
                 _out=Any(StringIO),
                 _err=Any(StringIO)
             )
+
+    # Given a shell command, 'bash'
+    @patch('sh.bash', create=True)
+    def test_success_false_when_shell_command_fails(self, bash_shell_command_mock):
+
+        # Given a working directory
+        with TempDirectory() as temp_dir:
+            working_dir_path = os.path.join(temp_dir.path, 'working')
+
+            config = {
+                'command': 'echooo "Hello World!"'
+            }
+
+            # Given an NpmPackage step implementer
+            ad_hoc_test = self.create_given_step_implementer(
+                AdHoc,
+                parent_work_dir_path=working_dir_path,
+                step_config=config
+            )
+
+            # Given the 'echooo Hello World!' shell command exits with an error code
+            bash_shell_command_mock.side_effect = sh.ErrorReturnCode('bash', b'mock stdout', b'mock error')
+
+            # When I run the step
+            step_result = ad_hoc_test.run_step()
+
+            # Then the StepResult should have success = False
+            self.assertEqual(step_result.success, False)
