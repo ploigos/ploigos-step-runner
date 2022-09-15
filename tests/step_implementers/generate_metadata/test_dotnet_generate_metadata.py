b19f59843036c9df3b0edced5a3b5a59d1a1605f
J T <jthompki@redhat.com>
2022-09-15 13:57:46 -0600

    Refactored a bit and added ability to specify version tag in config

--- tests/step_implementers/generate_metadata/test_dotnet_generate_metadata.py
+++ tests/step_implementers/generate_metadata/test_dotnet_generate_metadata.py
@@ -7,7 +7,7 @@ from unittest.mock import patch, call, PropertyMock
 from testfixtures import TempDirectory
 from tests.helpers.base_step_implementer_test_case import \
     BaseStepImplementerTestCase
-from ploigos_step_runner.step_implementers.generate_metadata import Dotnet
+from ploigos_step_runner.step_implementers.generate_metadata import DotnetGenerateMetadata
 from ploigos_step_runner.results import StepResult
 from ploigos_step_runner.exceptions import StepRunnerException
 
@@ -22,7 +22,7 @@ class TestStepImplementerDotnetGenerateMetadata(BaseStepImplementerTestCase):
             parent_work_dir_path=''
     ):
         return self.create_given_step_implementer(
-            step_implementer=Dotnet,
+            step_implementer=DotnetGenerateMetadata,
             step_config=step_config,
             step_name=step_name,
             implementer=implementer,
@@ -31,37 +31,16 @@ class TestStepImplementerDotnetGenerateMetadata(BaseStepImplementerTestCase):
         )
 
     def test_step_implementer_config_defaults(self):
-        defaults = Dotnet.step_implementer_config_defaults()
+        defaults = DotnetGenerateMetadata.step_implementer_config_defaults()
         expected_defaults = {
-            'csproj-file': 'dotnet-app.csproj'
+            'csproj-version-tag': 'Version'
         }
         self.assertEqual(defaults, expected_defaults)
 
     def test__required_config_or_result_keys(self):
-        required_keys = Dotnet._required_config_or_result_keys()
+        required_keys = DotnetGenerateMetadata._required_config_or_result_keys()
         expected_required_keys = ['csproj-file']
         self.assertEqual(required_keys, expected_required_keys)
-
-    def test__validate_required_config_or_previous_step_result_artifact_keys_package_file_does_not_exist(self):
-        with TempDirectory() as temp_dir:
-            parent_work_dir_path = os.path.join(temp_dir.path, 'working')
-            csproj_file_path = os.path.join(temp_dir.path, 'dotnet-app.csproj')
-            step_config = {
-                'csproj-file': csproj_file_path
-            }
-
-            step_implementer = self.create_step_implementer(
-                step_config=step_config,
-                step_name='test',
-                implementer='Dotnet',
-                parent_work_dir_path=parent_work_dir_path
-            )
-
-            with self.assertRaisesRegex(
-                    AssertionError,
-                    rf"Given csproj file \(csproj-file\) does not exist: {csproj_file_path}"
-            ):
-                step_implementer._validate_required_config_or_previous_step_result_artifact_keys()
 
     def test_run_step_pass(self):
 
@@ -87,7 +66,7 @@ class TestStepImplementerDotnetGenerateMetadata(BaseStepImplementerTestCase):
             step_implementer = self.create_step_implementer(
                 step_config=step_config,
                 step_name='generate-metadata',
-                implementer='Dotnet',
+                implementer='DotnetGenerateMetadata',
                 parent_work_dir_path=parent_work_dir_path,
             )
             # WHEN I run the step
@@ -97,8 +76,8 @@ class TestStepImplementerDotnetGenerateMetadata(BaseStepImplementerTestCase):
 
             expected_step_result = StepResult(
                 step_name='generate-metadata',
-                sub_step_name='Dotnet',
-                sub_step_implementer_name='Dotnet'
+                sub_step_name='DotnetGenerateMetadata',
+                sub_step_implementer_name='DotnetGenerateMetadata'
             )
             expected_step_result.add_artifact(name='app-version', value='1.2.0')
 
@@ -127,22 +106,52 @@ class TestStepImplementerDotnetGenerateMetadata(BaseStepImplementerTestCase):
             step_implementer = self.create_step_implementer(
                 step_config=step_config,
                 step_name='generate-metadata',
-                implementer='Dotnet',
+                implementer='DotnetGenerateMetadata',
                 parent_work_dir_path=parent_work_dir_path,
             )
 
             # WHEN I run the step
             result = step_implementer._run_step()
 
-            print(result)
-
             expected_step_result = StepResult(
                 step_name='generate-metadata',
-                sub_step_name='Dotnet',
-                sub_step_implementer_name='Dotnet',
+                sub_step_name='DotnetGenerateMetadata',
+                sub_step_implementer_name='DotnetGenerateMetadata',
             )
             expected_step_result.success = False
             expected_step_result.message = f"Could not get project version from given csproj file: ({csproj_file_path})"
+
+            # THEN the step should fail
+            self.assertEqual(result, expected_step_result)
+
+    def test_run_step_fail_missing_csproj_file(self):
+
+        # Given a missing XML file
+        with TempDirectory() as temp_dir:
+            parent_work_dir_path = os.path.join(temp_dir.path, 'working')
+            csproj_file_name = 'missingfile.csproj'
+
+            csproj_file_path = os.path.join(temp_dir.path, csproj_file_name)
+
+            step_config = {'csproj-file': csproj_file_path}
+
+            step_implementer = self.create_step_implementer(
+                step_config=step_config,
+                step_name='generate-metadata',
+                implementer='DotnetGenerateMetadata',
+                parent_work_dir_path=parent_work_dir_path,
+            )
+
+            # WHEN I run the step
+            result = step_implementer._run_step()
+
+            expected_step_result = StepResult(
+                step_name='generate-metadata',
+                sub_step_name='DotnetGenerateMetadata',
+                sub_step_implementer_name='DotnetGenerateMetadata',
+            )
+            expected_step_result.success = False
+            expected_step_result.message = f'Given csproj file (csproj-file) does not exist: {csproj_file_path}'
 
             # THEN the step should fail
             self.assertEqual(result, expected_step_result)
