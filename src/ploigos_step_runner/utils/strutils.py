e44634da1d9abfe5c513146eba5aa3ee3567f095
Christopher May <chmay@redhat.com>
2022-11-09 10:20:48 -0500

    Define strtobool method to replace the one from distutils

---
+++ src/ploigos_step_runner/utils/strutils.py
@@ -0,0 +1,19 @@
+"""Shared utilities for dealing with Strings
+"""
+
+def strtobool (val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+
+    NOTE: This is a modified version of strtobool from Python 3.9 distutils.
+    """
+    lower_val = val.lower()
+    if lower_val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif lower_val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError(f"invalid truth value {val}")
